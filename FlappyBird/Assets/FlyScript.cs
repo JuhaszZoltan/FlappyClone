079e9cc3b6bdf5c1c5814edc17e1cf0c533dc69f
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2019-11-07 15:00:57 +0100

    a

---
+++ FlappyBird/Assets/FlyScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyScript : MonoBehaviour
+{
+    public float velocity = 1;
+    public float rotationSmooth = 5;
+
+    Rigidbody2D rb;
+    Quaternion downRot;
+    Quaternion upRot;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        downRot = Quaternion.Euler(0, 0, -80);
+        upRot = Quaternion.Euler(0, 0, 40);
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * velocity;
+            transform.rotation = upRot;
+        }
+
+        transform.rotation = Quaternion.Lerp(
+            transform.rotation,
+            downRot,
+            rotationSmooth * Time.deltaTime);
+    }
+}
---
+++ FlappyBird/Assets/spawnPipe.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnPipe : MonoBehaviour
+{
+    public float maxTime = 2.5F;
+    float timer = 0;
+    public GameObject pipe;
+    public float height = 2;
+
+    void Start()
+    {
+        GameObject newPipe = Instantiate(pipe);
+        newPipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+        Destroy(newPipe, 15);
+    }
+
+    void Update()
+    {
+        if (timer > maxTime)
+        {
+            GameObject newPipe = Instantiate(pipe);
+            newPipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+            Destroy(newPipe, 15);
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+
+    void SpawnPipe()
+    {
+        
+    }
+}
